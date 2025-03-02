a15d22b35ccc4b8c8a725e9f4638c9f733f97d95
Artem Kalashnikov <kalashnikov@petroleum.digital>
2025-03-02 19:18:40 +0300

    iter2

--- internal/repository/link_test.go
+++ internal/repository/link_test.go
@@ -1,4 +1,4 @@
-package postgres_test
+package repository_test
 
 import (
 	"context"
@@ -7,15 +7,33 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/ypxd99/yandex-practicm/internal/repository"
 	"github.com/ypxd99/yandex-practicm/internal/repository/postgres"
+	"github.com/ypxd99/yandex-practicm/internal/repository/storage"
+	"github.com/ypxd99/yandex-practicm/util"
 )
 
 func TestCreateAndFindLink(t *testing.T) {
+	cfg := util.GetConfig()
+	util.InitLogger(cfg.Logger)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	testID := "test123"
 	testURL := "https://example.com"
+	var repo repository.LinkRepository
+	if cfg.Postgres.UsePostgres {
+		postgresRepo, err := postgres.Connect(context.Background())
+		if err != nil {
+			t.Fatalf("Failed to initialize Postgres: %v", err)
+		}
+		repo = postgresRepo
+	} else {
+		repo = storage.InitStorage()
+	}
+	defer repo.Close()
+
 	db, err := postgres.Connect(ctx)
 	assert.NoError(t, err)
 
