b60c4284b2f91ee6797506801db0da54f6f97b13
Artem Kalashnikov <kalashnikov@petroleum.digital>
2025-03-18 12:31:56 +0300

    iter6

--- internal/repository/middleware/logging.go
+++ internal/repository/middleware/logging.go
@@ -1,11 +1,8 @@
 package middleware
 
 import (
-	"bufio"
 	"bytes"
 	"io"
-	"net"
-	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,15 +10,13 @@ import (
 	"github.com/ypxd99/yandex-practicm/util"
 )
 
-var noWritten = -1
-
 type responseData struct {
 	status int
 	size   int
 }
 
 type loggingResponseWriter struct {
-	http.ResponseWriter
+	gin.ResponseWriter
 	responseData *responseData
 }
 
@@ -34,59 +29,6 @@ func (r *loggingResponseWriter) Write(b []byte) (int, error) {
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
 	r.ResponseWriter.WriteHeader(statusCode)
 	r.responseData.status = statusCode
-}
-
-func (r *loggingResponseWriter) CloseNotify() <-chan bool {
-	if cn, ok := r.ResponseWriter.(http.CloseNotifier); ok {
-		return cn.CloseNotify()
-	}
-	return nil
-}
-
-func (r *loggingResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	if r.responseData.size < 0 {
-		r.responseData.size = 0
-	}
-	return r.ResponseWriter.(http.Hijacker).Hijack()
-}
-
-func (r *loggingResponseWriter) Flush() {
-	if fl, ok := r.ResponseWriter.(http.Flusher); ok {
-		fl.Flush()
-	}
-}
-
-func (r *loggingResponseWriter) Pusher() (pusher http.Pusher) {
-	if pusher, ok := r.ResponseWriter.(http.Pusher); ok {
-		return pusher
-	}
-	return nil
-}
-
-func (r *loggingResponseWriter) WriteHeaderNow() {
-	if !r.Written() {
-		r.responseData.size = 0
-		r.ResponseWriter.WriteHeader(r.responseData.status)
-	}
-}
-
-func (r *loggingResponseWriter) WriteString(s string) (n int, err error) {
-	r.WriteHeaderNow()
-	n, err = io.WriteString(r.ResponseWriter, s)
-	r.responseData.size += n
-	return
-}
-
-func (r *loggingResponseWriter) Status() int {
-	return r.responseData.status
-}
-
-func (r *loggingResponseWriter) Size() int {
-	return r.responseData.size
-}
-
-func (r *loggingResponseWriter) Written() bool {
-	return r.responseData.size != noWritten
 }
 
 func LoggingMiddleware() gin.HandlerFunc {
