8b6ca96cd9d544c36381ced5356dcc2e629033cf
Artem Kalashnikov <kalashnikov@petroleum.digital>
2025-07-15 11:58:29 +0300

    iter23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -79,7 +79,7 @@ func main() {
 	}()
 
 	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	<-quit
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
